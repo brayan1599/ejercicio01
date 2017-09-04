bc0378e7c16766142f033528cbad8cd0d653ac31
alumno <alumno@LIMSFLAB06-12D.system-uap.adm>
2017-09-05 05:21:01 +0600

    first commint

---
+++ src/ejercicio01_01/Ejecicico01_01.java
@@ -0,0 +1,15 @@
+package ejercicio01_01;
+
+/*(Display three messages)Write a progam that displays Welcome to java,
+ welcome to -computer Science, and Progamming is fun.
+*/
+public class Ejecicico01_01{
+    public static void main(String[] args){
+        System.out.println("bienvenido a java");
+        System.out.println("bienvenido a computer Science");
+        System.out.println("Programming is fun");
+    
+    }
+    
+}    
+
